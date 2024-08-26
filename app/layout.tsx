136cf3c99046de1b6b7da8de14c2db7286bbf562
Hamza Y <yousfihamza22@gmail.com>
2024-08-26 17:08:24 +0200

    feat: Metadata Tags for SEO and preview Links

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,59 @@ import { ThemeToggle } from "@/components/ui/ThemeToggle";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 
+export const metadata: Metadata = {
+  metadataBase: new URL("https://vitalpath.yousfi.dev"),
+  title: "VitalPath | Leading Healthcare Management Platform",
+  description:
+    "VitalPath is your gateway to modern healthcare management. Book appointments, track health, and receive personalized care updates effortlessly.",
+  keywords: [
+    "healthcare",
+    "management",
+    "appointments",
+    "health tracking",
+    "VitalPath",
+    "medical platform",
+    "patient management",
+    "telemedicine",
+    "online healthcare",
+    "digital health",
+    "health monitoring",
+    "virtual care",
+    "health services",
+    "personalized care",
+    "health technology",
+  ],
+  openGraph: {
+    title: "VitalPath | Leading Healthcare Management Platform",
+    description:
+      "VitalPath is your gateway to modern healthcare management. Book appointments, track health, and receive personalized care updates effortlessly.",
+    type: "website",
+    url: "https://vitalpath.yousfi.dev",
+    images: [
+      {
+        url: "/preview.jpg",
+        width: 789,
+        height: 500,
+        alt: "VitalPath - Healthcare Management",
+      },
+    ],
+    siteName: "VitalPath",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "VitalPath | Leading Healthcare Management Platform",
+    description:
+      "VitalPath is your gateway to modern healthcare management. Book appointments, track health, and receive personalized care updates effortlessly.",
+    images: [
+      {
+        url: "/preview.jpg",
+        alt: "VitalPath - Healthcare Management",
+      },
+    ],
+  },
+  applicationName: "VitalPath",
+};
+
 const inter = Inter({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
@@ -18,12 +71,6 @@ const sans = Plus_Jakarta_Sans({
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-sans",
 });
-
-export const metadata: Metadata = {
-  title: "CarePluse",
-  description: "Patient Management System",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
